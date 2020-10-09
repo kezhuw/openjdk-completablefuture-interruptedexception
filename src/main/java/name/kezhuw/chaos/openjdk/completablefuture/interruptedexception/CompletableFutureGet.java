31cc3ac1e1ba9b050ac5825f2faee0af48f57d7d
Kezhu Wang <kezhuw@gmail.com>
2020-10-09 21:52:09 +0800

    Simplify thread models for demonstration code

--- src/main/java/name/kezhuw/chaos/openjdk/completablefuture/interruptedexception/CompletableFutureGet.java
+++ src/main/java/name/kezhuw/chaos/openjdk/completablefuture/interruptedexception/CompletableFutureGet.java
@@ -5,12 +5,12 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CompletableFutureGet {
     private static final PrintStream stdout = System.out;
     private static final PrintStream stderr = System.err;
 
+    private static final String futureMethod;
     private static final FutureWaiter futureWaiter;
 
     private static final int maxRuns;
@@ -18,8 +18,10 @@ public class CompletableFutureGet {
     static {
         String method = System.getenv("FUTURE_WAIT_METHOD");
         if (method == null || method.equalsIgnoreCase("get")) {
+            futureMethod = "get";
             futureWaiter = CompletableFuture::get;
         } else if (method.equalsIgnoreCase("join")) {
+            futureMethod = "join";
             futureWaiter = CompletableFuture::join;
         } else {
             String msg = String.format("Invalid FUTURE_WAIT_METHOD value %s, candidates are get and join", method);
@@ -45,52 +47,35 @@ public class CompletableFutureGet {
     }
 
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i < maxRuns; i++) {
+        for (int i = 1; i <= maxRuns; i++) {
             long sleepMills = ThreadLocalRandom.current().nextLong(10);
-            stdout.format("%d: sleep mills: %d\n", i, sleepMills);
+            final String sleepString = sleepMills == 0 ? "--------" : String.format("sleep(%d)", sleepMills);
+            final String prefix = String.format("%4d/%d interrupt-%s-complete", i, maxRuns, sleepString);
 
             CompletableFuture<Void> future = new CompletableFuture<>();
-            AtomicBoolean interrupted = new AtomicBoolean();
 
-            CountDownLatch readyLatch = new CountDownLatch(2);
-            CountDownLatch startLatch = new CountDownLatch(1);
-
-            Thread futureCompleteThread = new Thread(() -> {
-                try {
-                    readyLatch.countDown();
-                    startLatch.await();
-                    future.complete(null);
-                } catch (InterruptedException ex) {
-                    stdout.println("Got unexpected interrupted exception in future complete thread");
-                    ex.printStackTrace();
-                    System.exit(128);
-                }
-            });
-            futureCompleteThread.setDaemon(true);
-            futureCompleteThread.start();
-
+            CountDownLatch waitingFutureLatch = new CountDownLatch(1);
             CountDownLatch futureGotLatch = new CountDownLatch(1);
+
             Thread futureGetThread = new Thread(() -> {
                 try {
+                    waitingFutureLatch.countDown();
                     futureWaiter.wait(future);
-                    while (!interrupted.get()) {
-                        Thread.yield();
-                    }
                     // XXX: Test whether interrupt status was lost.
                     if (Thread.currentThread().isInterrupted()) {
-                        stdout.println("Future get thread was interrupted.");
+                        stdout.format("%s: future.%s completes, Thread.isInterrupted returns true\n", prefix, futureMethod);
                     } else {
-                        stderr.println("Future get thread lost interrupt status");
+                        stderr.format("%s: future.%s completes, Thread.isInterrupted returns false\n", prefix, futureMethod);
                         System.exit(1);
                     }
                 } catch (InterruptedException ex) {
-                    // Thread.currentThread().interrupt();
-                    stdout.println("future.get() got interrupted");
+                    stdout.format("%s: future.%s is interrupted.\n", prefix, futureMethod);
                     try {
                         futureWaiter.wait(future);
                     } catch (Exception ex1) {
                         stderr.println("Got unexpected exception");
                         ex.printStackTrace();
+                        System.exit(128);
                     }
                 } catch (ExecutionException ex) {
                     stderr.println("Got unexpected execution exception");
@@ -98,28 +83,23 @@ public class CompletableFutureGet {
                     System.exit(128);
                 }
                 futureGotLatch.countDown();
-            });
+            }, String.format("future-get-thread-%d", i));
             futureGetThread.setDaemon(true);
             futureGetThread.start();
 
-            Thread interruptingThread = new Thread(() -> {
-                try {
-                    readyLatch.countDown();
-                    startLatch.await();
-                    if (sleepMills > 0) {
-                        Thread.sleep(sleepMills);
-                    }
-                    futureGetThread.interrupt();
-                    interrupted.set(true);
-                } catch (InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                }
-            });
-            interruptingThread.setDaemon(true);
-            interruptingThread.start();
+            waitingFutureLatch.await();
+            Thread.sleep(1);
 
-            readyLatch.await();
-            startLatch.countDown();
+            try {
+                futureGetThread.interrupt();
+                if (sleepMills > 0) {
+                    Thread.sleep(sleepMills);
+                }
+                future.complete(null);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+
             futureGotLatch.await();
         }
     }
